ac9e5062ba7abf8c14c7522bcd852c9bcc767d46
Ashli Rankin <ashlirankin18@gmail.com>
2023-12-04 12:53:04 -0500

    Added new Xcode project
    Signed-off-by: Ashli Rankin <ashlirankin18@gmail.com>

---
+++ Simply/Simply/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simply
+//
+//  Created by Ashli Rankin on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simply/Simply/SimplyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyApp.swift
+//  Simply
+//
+//  Created by Ashli Rankin on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SimplyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
